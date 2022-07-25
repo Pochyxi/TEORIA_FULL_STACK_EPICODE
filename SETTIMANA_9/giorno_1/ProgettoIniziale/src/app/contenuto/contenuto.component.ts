b316824555b1f1170449e3bf262e993a0a2b3201
Pochyxi <adienerlopez@gmail.com>
2022-07-25 13:42:14 +0200

    commit in master

---
+++ SETTIMANA_9/giorno_1/ProgettoIniziale/src/app/contenuto/contenuto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contenuto',
+  templateUrl: './contenuto.component.html',
+  styleUrls: ['./contenuto.component.scss']
+})
+export class ContenutoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
